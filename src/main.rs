c6724405c13eff6dde7182696f652f4331c4b2c6
angelina-tsuboi <angelina.t1832@gmail.com>
2021-10-14 23:34:05 -0700

    feat: make initial cli program with path inputs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
 struct Cli {
     pattern: String,
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
+    // parses args into our CLI struct
+    let args = Cli::from_args();
     println!("Hello, world!");
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
