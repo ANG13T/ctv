9ebc50987a851c7e28b7fe8266f4b51467966c54
angelina-tsuboi <angelina.t1832@gmail.com>
2021-11-03 14:56:48 -0700

    feat: added file and dir scanning

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,73 @@
 use structopt::StructOpt;
 use anyhow::{Context, Result};
+use std::{env, fs};
+use  std::fs::metadata;
+// mod input;
+// mod text_effects;
+// mod utils;
+use std::os::unix::fs::{FileTypeExt, MetadataExt};
+use std::path::Path;
+// use structopt::StructOpt;
+use std::cmp::Ordering;
+
+// struct Directory {
+//     paths: Vec<File>,
+//     args: input::Cli,
+// }
+  
+// #[derive(Clone)]
+// struct File {
+//   path:      std::path::PathBuf,
+//   file_type: Vec<PathType>,
+//   group:     String,
+//   user:      String,
+//   modified:  String,
+//   created:   String,
+//   size:      String,
+//   perms:     String,
+// }
+
+// fn printDirAndFilesForDir() -> Array<File|Directory>{
+
+// }
+
+// fn printFileString(input : File) -> String {
+//     return "";
+// }
+
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 // using Context from anyhow library to provide context for error messages.  it also keeps the original error, so we get a “chain” of error messages pointing out the root cause.
 
 fn main() -> Result<()>{
-    // read file from string
-    // question mark at end automatically returns an error if path is incorrect
-    let path = "test.txt";
-    let result = std::fs::read_to_string(path)
-    .with_context(|| format!("could not read file `{}`", path))?;
-    println!("file content: {}", content);
+    let current_dir = env::current_dir()?;
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+
+    for entry in fs::read_dir(current_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        let metadata = fs::metadata(&path)?;
+        let last_modified = metadata.modified()?.elapsed()?.as_secs();
+
+        if last_modified < 24 * 3600 && metadata.is_file() {
+            println!(
+                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
+                last_modified,
+                metadata.permissions().readonly(),
+                metadata.len(),
+                path.file_name().ok_or("No filename")
+            );
+        }
+    }
+
     Ok(())
 }
