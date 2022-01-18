691403291827bbc338fba4d3dbdb00110e2eafb2
angelina-tsuboi <angelina.t1832@gmail.com>
2022-01-17 19:00:10 -0800

    feat: set env from cli working

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ fn main() -> Result<(), Box<dyn Error>>{
     if !protocols::checkenv::check_env() {
         Err("ENV variables not declared properly")?
     }
+    modify_env_with_flags();
     let env_manager = protocols::EnvManager::init();
-    let updated_env_manager = modify_env_with_flags(env_manager);
-    let mut dir_tree = protocols::DirTree::init(input::Cli::from_args().dir, updated_env_manager);
+    let mut dir_tree = protocols::DirTree::init(input::Cli::from_args().dir, env_manager);
     dir_tree.gen();
     Ok(())
 }
 
-fn modify_env_with_flags(env_manager: protocols::EnvManager) -> protocols::EnvManager{
+fn modify_env_with_flags() {
     let set_env: &str = &input::Cli::from_args().set_env.clone();
+    println!("modify!! {}", set_env);
     if set_env != "" && check_valid_set_env(set_env.to_string()){
         set_env_var(&set_env);
     }
-    return env_manager;
 }
 
 
@@ -34,6 +34,10 @@ fn set_env_var(env_string: &str){
 fn check_valid_set_env(env_input: String) -> bool{
     let used_positions: Vec<String> = protocols::checkenv::get_used_positions();
     let string_vec: Vec<&str> = env_input.split("=").collect();
+    if string_vec.len() != 2{
+        println!("ERROR: invalid flag variable for --set-env");
+        return false;
+    }
     return protocols::checkenv::check_env_var(string_vec[0], string_vec[1], &used_positions);
 }
 
