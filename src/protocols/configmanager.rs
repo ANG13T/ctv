5a64dab14a2a897668b6e0a86b4783e9e760d51e
angelina-tsuboi <angelina.t1832@gmail.com>
2022-01-20 14:54:27 -0800

    feat: remove config vars

--- src/protocols/configmanager.rs
+++ src/protocols/configmanager.rs
@@ -1,6 +1,6 @@
 extern crate dotenv;
 use dotenv::dotenv;
-use std::{env, fs};
+use std::{env};
 use load_dotenv::load_dotenv;
 
 #[derive(Debug)]
@@ -51,76 +51,6 @@ pub struct EnvManager {
     pub show_short: bool
 }
 
-
-fn configure_variables(){
-    // if let Some(proj_dirs) = ProjectDirs::from(
-    //     "com",
-    //     "angelina-tsuboi",
-    //     "ctv",
-    // ) {
-    //     let config_dir = proj_dirs.config_dir();
-
-    //     dbg!(config_dir);
-
-    //     let config_file = fs::read_to_string(
-    //         config_dir.join("config.toml"),
-    //     );
-
-    //     println!("{}", config_file.unwrap());
-
-        // let config = match config_file {
-        //     Ok(file) => toml::from_str(&file).unwrap(),
-        //     Err(_) => EnvManager {
-        //      file_size_position: 1,
-        //      file_owner_position: 2,
-        //      file_perms_position: 3,
-        //      file_time_position: 4,
-        //      file_extension_position: -1,
-        //      dir_name_color: "LIGHTREDDDDDDD".to_string(),
-        //      file_name_color: "LIGHTRED".to_string(),
-        //      file_time_color: "LIGHTCYAN".to_string(),
-        //      file_size_color: "BLUE".to_string(),
-        //      file_owner_color: "MAGENTA".to_string(),
-        //      file_perms_color: "BLUE".to_string(),
-        //      file_extension_color: "YELLOW".to_string(),
-        //      dir_name_style: "LIGHTCYAN".to_string(),
-        //      file_name_style: "NORMAL".to_string(),
-        //      file_time_style: "BOLD".to_string(),
-        //      file_size_style: "BOLD".to_string(),
-        //      file_owner_style: "NORMAL".to_string(),
-        //      file_perms_style: "BOLD".to_string(),
-        //      file_extension_style: "ITALIC".to_string(),
-        //      file_time_format: "%m-%d-%Y::%H:%M:%S".to_string(),
-        //      file_time_type: "CREATED".to_string(),
-        //      tree_layer_limit: 3,
-        //      show_file_metadata: "TRUE".to_string(),
-        //      show_dir_metadata: "TRUE".to_string(),
-        //      pipe: "│".to_string(),
-        //      elbow: "└──".to_string(),
-        //      tee: "├──".to_string(),
-        //      pipe_prefix: "│".to_string(),
-        //      space_prefix: " ".to_string(),
-        //      num_positions: 4,
-        //      dir_num_positions: 4,
-        //      dir_color: "BLUE".to_string(),
-        //      symlink_color: "LIGHTMAGENTA".to_string(),
-        //      path_color: "WHITE".to_string(),
-        //      pipe_color: "YELLOW".to_string(),
-        //      chard_color: "YELLOW".to_string(),
-        //      blockd_color: "LIGHTGREEN".to_string(),
-        //      socket_color: "LIGHTRED".to_string(),
-        //      read_color: "LIGHTGREEN".to_string(),
-        //      write_color: "LIGHTRED".to_string(),
-        //      execute_color: "LIGHTGREEN".to_string(),
-        //      dash_color: "LIGHTBLACK".to_string(),
-        //      spacing: 0,
-        //      show_short: false
-        //     }
-        // };
-
-        // dbg!(config);
-    
-}
 
 impl EnvManager {
     pub fn init() -> Self {
